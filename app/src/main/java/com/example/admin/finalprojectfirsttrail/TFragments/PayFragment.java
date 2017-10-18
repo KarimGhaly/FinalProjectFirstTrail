83278454ec91da0bb00bf311f5d9315515a1e35a
Edel <edelalexanderfb@gmail.com>
2017-10-18 03:15:55 -0400

    Added Functionality to Button                  - Report Expense

--- app/src/main/java/com/example/admin/finalprojectfirsttrail/TFragments/PayFragment.java
+++ app/src/main/java/com/example/admin/finalprojectfirsttrail/TFragments/PayFragment.java
@@ -2,13 +2,17 @@ package com.example.admin.finalprojectfirsttrail.TFragments;
 
 
 import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,12 +26,17 @@ import com.example.admin.finalprojectfirsttrail.FragmentClass.PayStubFragClass;
 import com.example.admin.finalprojectfirsttrail.InfoClass.AdvanceInfoClass;
 import com.example.admin.finalprojectfirsttrail.R;
 import com.example.admin.finalprojectfirsttrail.RecyclerViewApadpters.PaySlipViewPagerAdapter;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.example.admin.finalprojectfirsttrail.InfoClass.*;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Date;
 import java.util.Objects;
@@ -37,11 +46,15 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 
+import static android.app.Activity.RESULT_OK;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PayFragment extends Fragment {
     public static final String TAG = "PayFragmentTAG";
+    private static final int GALLERY_INTENT = 12;
+
     PayStubFragClass payStubFragClass;
     @BindView(R.id.payStubTitle)
     TextView payStubTitle;
@@ -66,6 +79,7 @@ public class PayFragment extends Fragment {
     FirebaseDatabase database;
     DatabaseReference ref;
     private String uid;
+    private int gallery_intent;
 
 
     public PayFragment() {
@@ -136,6 +150,7 @@ public class PayFragment extends Fragment {
 
     }
 
+    //////////////////////////////////////////////////////////////NEW
     public void SubmitExpense() {
         final Dialog SEDialog = new Dialog(getContext());
         SEDialog.setTitle("Submit Expense");
@@ -153,7 +168,10 @@ public class PayFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 flag[0] = true;
-                ///////INSERT IMAGE TO DATABASE
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_INTENT);
             }
         });
         btnSubmitExpense.setOnClickListener(new View.OnClickListener() {
@@ -166,7 +184,7 @@ public class PayFragment extends Fragment {
                         ExpenseInfoClass expense = new ExpenseInfoClass();
                         expense.setAmount(Float.valueOf(etAmount.getText().toString()));
                         expense.setDescription(etDescription.getText().toString());
-                    }else{
+                    } else {
                         Toast.makeText(getContext(), "Insert Receipt", Toast.LENGTH_SHORT).show();
                     }
                 }
@@ -196,10 +214,38 @@ public class PayFragment extends Fragment {
                 RequestAdvance();
                 break;
             case R.id.btnPayStubFrag_expenseReport:
-                SubmitExpense();
+                SubmitExpense();//////////////NEW
                 break;
             case R.id.btnPayStubFrag_submitExpense:
                 break;
+        }
+    }
+
+    ////////////NEW
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+            Uri uri = data.getData();
+
+            StorageReference mStorageRef = FirebaseStorage.getInstance().getReference();
+            StorageReference filepath = mStorageRef.child("Photos").child(uri.getLastPathSegment());
+            //StorageReference filepath = mStorageRef.child("Edel").child(uri.getPath());
+
+            filepath.putFile(uri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(getContext(), "Upload Successful", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(), "Upload Unsuccessful", Toast.LENGTH_LONG).show();
+                            Log.d(TAG, "onFailure: "+ e);
+                        }
+                    });
         }
     }
 }
