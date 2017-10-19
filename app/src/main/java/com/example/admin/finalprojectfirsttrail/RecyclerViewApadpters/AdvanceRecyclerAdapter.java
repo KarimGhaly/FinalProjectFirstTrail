14d179cb9d3d39f031ebc1d228c2d11bb91654f9
Karim Ghaly <ghaly.karim@outlook.com>
2017-10-19 14:33:27 -0400

    View Advances Requests

--- app/src/main/java/com/example/admin/finalprojectfirsttrail/RecyclerViewApadpters/AdvanceRecyclerAdapter.java
+++ app/src/main/java/com/example/admin/finalprojectfirsttrail/RecyclerViewApadpters/AdvanceRecyclerAdapter.java
@@ -10,14 +10,18 @@ import android.widget.TextView;
 import com.example.admin.finalprojectfirsttrail.InfoClass.AdvanceInfoClass;
 import com.example.admin.finalprojectfirsttrail.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by ios on 10/18/17.
  */
 
-public class AdvanceRecyclerAdapter  extends RecyclerView.Adapter<AdvanceRecyclerAdapter.ViewHolder>{
+public class AdvanceRecyclerAdapter extends RecyclerView.Adapter<AdvanceRecyclerAdapter.ViewHolder> {
 
     private static final String TAG = "RecyclerViewAdapter";
 
@@ -28,32 +32,44 @@ public class AdvanceRecyclerAdapter  extends RecyclerView.Adapter<AdvanceRecycle
         this.myList = myList;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
-            super(itemView);
-            //bind the views in the custom item layout
-        }
-    }
 
     @Override
-    public AdvanceRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         context = parent.getContext();
-
-        View view = LayoutInflater
-                .from(context)
-                .inflate(R.layout.alert_recycler_view, parent,false);
-        return new AdvanceRecyclerAdapter.ViewHolder(view);
+        View view = LayoutInflater.from(context).inflate(R.layout.alert_recycler_view, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        AdvanceInfoClass advance = myList.get(position);
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        holder.txtRecyclerADVDate.setText(df.format(advance.getDate()));
+        holder.txtRecyclerADVAmount.setText(String.valueOf(advance.getAmount()));
+        holder.txtRecyclerADVDesc.setText(advance.getStatus());
+        holder.txtRecyclerADVDesc.setText(advance.getDescriction());
     }
 
     @Override
     public int getItemCount() {
 
         return myList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.txtRecyclerADV_Date)
+        TextView txtRecyclerADVDate;
+        @BindView(R.id.txtRecyclerADV_Status)
+        TextView txtRecyclerADVStatus;
+        @BindView(R.id.txtRecyclerADV_Amount)
+        TextView txtRecyclerADVAmount;
+        @BindView(R.id.txtRecyclerADV_Desc)
+        TextView txtRecyclerADVDesc;
+
+        ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
     }
 }
 
